46f632025f00581396fa5c58717bae3bfa0abf50
himanshuhimmy <122083033+himanshuhimmy@users.noreply.github.com>
2025-08-29 17:29:14 +0530

    userInput

--- src/components/DietData/DietRoot.jsx
+++ src/components/DietData/DietRoot.jsx
@@ -1,69 +1,95 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import DietInput from "./DietInput";
 import DisplayDiet from "./DisplayDiet";
+import UserInput from "./UserInput";
 
 const DietRoot = () => {
   let [display, setDisplay] = useState(false);
-  let [meals, setMeals] = useState([
+  // let [meals, setMeals] = useState([
+  //   {
+  //     mealName: "Breakfast",
+  //     time: 0,
+  //     status: true,
+  //     item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  //   },
+  //   {
+  //     mealName: "Brunch",
+  //     time: 0,
+  //     status: true,
+  //     item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  //   },
+  //   {
+  //     mealName: "Lunch",
+  //     time: 0,
+  //     status: true,
+  //     item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  //   },
+  //   {
+  //     mealName: "Snack",
+  //     time: 0,
+  //     status: true,
+  //     item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  //   },
+  //   {
+  //     mealName: "PreWorkout",
+  //     time: 0,
+  //     status: true,
+  //     item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  //   },
+  // {
+  //   mealName: "Dinner",
+  //   time: 0,
+  //   status: true,
+  //   item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+  // },
+  // ]);
+
+  let [name, setName] = useState([{ UserName: ``, mealNo: 0 }]);
+  console.log(name);
+  let [userMeals, setUserMeals] = useState([
     {
-      mealName: "Breakfast",
+      mealName: "aaa",
       time: 0,
+      id: 1,
       status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
-    },
-    {
-      mealName: "Brunch",
-      time: 0,
-      status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
-    },
-    {
-      mealName: "Lunch",
-      time: 0,
-      status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
-    },
-    {
-      mealName: "Snack",
-      time: 0,
-      status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
-    },
-    {
-      mealName: "PreWorkout",
-      time: 0,
-      status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
-    },
-    {
-      mealName: "Dinner",
-      time: 0,
-      status: true,
-      item: [{ id: 1, Food: "", time: 0, Carbs: 0, Fats: 0, Protien: 0 }],
+      item: [{ id: 1, Food: "", Carbs: 0, Fats: 0, Protien: 0 }],
     },
   ]);
+  console.log(userMeals);
 
   function toggle() {
     setDisplay(!display);
   }
+
   return (
     <div className="text-center p-9">
       <header className="text-4xl text-cyan-600 font-semibold">
         Your Daily Food
       </header>
       <div className="m-5 p-5">
-        <h1 className="text-2xl text-cyan-700 font-semibold">
+        <div className="bg-cyan-700 rounded-xl p-4">
+          <div className="p-4">
+            <UserInput
+              name={name}
+              SetName={setName}
+              meals={userMeals}
+              setMeals={setUserMeals}
+              toggle={toggle}
+            />
+          </div>
+        </div>
+        {/* <h1 className="text-2xl text-cyan-700 font-semibold">
           {display ? `Your Meals ` : ` Enter your Meals `}
-        </h1>
+        </h1> */}
       </div>
 
-      <div className="border p-4 m-2 rounded-md">
+      {/* <div className="border p-4 m-2 rounded-md">
         {display ? (
           <DisplayDiet meal={meals} toggle={toggle} />
         ) : (
           <DietInput meals={meals} toggle={toggle} setMeals={setMeals} />
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/components/DietData/UserInput.jsx
+++ src/components/DietData/UserInput.jsx
@@ -1,7 +1,330 @@
-import React from "react";
+import { useState } from "react";
+import DisplayDiet from "./DisplayDiet";
 
-const UserInput = () => {
-  return <div></div>;
+const UserInput = ({ meals, setMeals, name, SetName, toggle }) => {
+  let inputBannerClass = " p-3 text-cyan-400";
+  let inputClass = "rounded-lg p-1 mb-2 text-cyan-600";
+  let buttonClass =
+    "bg-cyan-600 p-4 rounded-xl text-white  hover:bg-cyan-500 transition-all duration-500";
+
+  let [navInput, setNavInput] = useState(3);
+
+  function setName(e) {
+    let initialSate = [...name];
+    let update = initialSate.map((el) => ({ ...el, UserName: e.target.value }));
+    SetName(update);
+  }
+  function generateMeals(count) {
+    return Array.from({ length: count }, (_, i) => ({
+      mealName: "",
+      id: i + 1,
+      time: 0,
+      status: true,
+      item: [{ id: 1, Food: "", Carbs: 0, Fats: 0, Protein: 0 }],
+    }));
+  }
+
+  function setNoMeal(e) {
+    let initialSate = [...name];
+    const mealCount = parseInt(e.target.value);
+    let update = initialSate.map((el) => ({ ...el, mealNo: mealCount }));
+    const newMeals = generateMeals(mealCount);
+    setMeals(newMeals);
+    SetName(update);
+  }
+
+  function onNextHandle(event) {
+    event.preventDefault();
+    if (navInput === 4) {
+      setNavInput(1);
+    } else {
+      setNavInput(navInput + 1);
+    }
+  }
+  function onPrevHandle() {
+    setNavInput(navInput - 1);
+  }
+
+  // update meal name and time
+  function MealName(Event, id) {
+    let initalSate = [...meals];
+    let userMeal = Event.target.value;
+    let update = initalSate.map((el) =>
+      el.id === id ? { ...el, mealName: userMeal } : el
+    );
+    setMeals(update);
+  }
+  function MealTime(Event, id) {
+    let initalSate = [...meals];
+    let userTime = Event.target.value;
+    let update = initalSate.map((el) =>
+      el.id === id ? { ...el, time: userTime } : el
+    );
+    setMeals(update);
+  }
+
+  // add and remove item
+
+  function handleAddItem(Mid, Iid) {
+    let initalSate = [...meals];
+    let update = initalSate.map((Meals) => {
+      if (Meals.id === Mid) {
+        return {
+          ...Meals,
+          item: [
+            ...Meals.item,
+            {
+              id: Meals.item.length + 1,
+              Food: "",
+              Carbs: 0,
+              Fats: 0,
+              Protien: 0,
+            },
+          ],
+        };
+      }
+      return Meals;
+    });
+    setMeals(update);
+  }
+  function handleRemoveItem(Mid, Uid) {
+    let initalSate = [...meals];
+
+    let update = initalSate.map((Meals) => {
+      if (Meals.id === Mid) {
+        return { ...Meals, item: Meals.item.filter((el) => el.id !== Uid) };
+      }
+      return Meals;
+    });
+    setMeals(update);
+  }
+
+  // update Items
+  function onChangeHandle(field, value, Mid, Iid) {
+    let initalSate = [...meals];
+    let update = initalSate.map((Meals) => {
+      if (Meals.id === Mid) {
+        return {
+          ...Meals,
+          item: Meals.item.map((el) =>
+            el.id === Iid ? { ...el, [field]: parseInt(value) } : el
+          ),
+        };
+      }
+      return Meals;
+    });
+    setMeals(update);
+    toggle();
+  }
+
+  return (
+    <div
+      className={` bg-cyan-700 rounded-xl p-4 ${
+        navInput === 3 ? ` w-[90%]` : ` w-[30%]`
+      }  m-auto `}
+    >
+      <form onSubmit={onNextHandle}>
+        {navInput === 1 && (
+          <div>
+            {name.map((el) => {
+              return (
+                <div>
+                  <div className="flex-col ">
+                    <p className={inputBannerClass}>Enter Your Full Name</p>
+                    <input
+                      value={el.UserName}
+                      required
+                      minLength={3}
+                      className={inputClass}
+                      placeholder="Full Name"
+                      type="text"
+                      onChange={(e) => setName(e)}
+                    />
+                  </div>
+                  <div className="flex-col mb-4">
+                    <p className={inputBannerClass}>
+                      Total No Of Meals You Have ?
+                    </p>
+                    <input
+                      value={el.mealNo}
+                      required
+                      className={inputClass}
+                      placeholder="Meals ?"
+                      type="number"
+                      onChange={(e) => setNoMeal(e)}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {navInput === 2 && (
+          <div className="mb-3 p-2">
+            {meals.map((el) => {
+              return (
+                <div>
+                  <div className="flex-col">
+                    <p className={inputBannerClass}>Enter Your Meal Names </p>
+                    <input
+                      value={el.mealName}
+                      className={inputClass}
+                      required
+                      minLength={3}
+                      placeholder={`meal no ${el.id}`}
+                      type="text"
+                      onChange={(e) => MealName(e, el.id)}
+                    />
+                  </div>
+                  <div className="flex-col">
+                    <p className={inputBannerClass}>Enter Your Meal Timing </p>
+                    <input
+                      value={el.time}
+                      className={inputClass}
+                      required
+                      type="time"
+                      onChange={(e) => MealTime(e, el.id)}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+        {navInput === 3 && (
+          <div className="w-full">
+            {meals.map((Meals) => {
+              return (
+                <div key={Meals.id}>
+                  <div className="flex justify-center mb-5">
+                    <h1 className="text-white font-semibold text-xl mx-3">
+                      {Meals.mealName}
+                    </h1>
+                    <h1 className="text-white  font-semibold text-xl mx-3">
+                      Time:- {Meals.time}
+                    </h1>
+                  </div>
+                  {Meals.item.map((Item) => {
+                    return (
+                      <div key={Item.id} className="flex-col ">
+                        <div>
+                          <div className="flex justify-around w-[20%] m-auto mt-3">
+                            <p className="text-white font-semibold">
+                              Food Item :-{" "}
+                            </p>
+
+                            <input
+                              value={Item.Food === 0 ? `` : Item.Food}
+                              onChange={(e) =>
+                                onChangeHandle(
+                                  "Food",
+                                  e.target.value,
+                                  Meals.id,
+                                  Item.id
+                                )
+                              }
+                              placeholder="Food "
+                              className={inputClass}
+                              required
+                              type="text"
+                            />
+                          </div>
+                          <div className="flex justify-around w-[25%] m-auto p-3">
+                            <button
+                              onClick={() => handleAddItem(Meals.id, Item.id)}
+                              className={buttonClass}
+                            >
+                              Add Item
+                            </button>
+                            <button
+                              onClick={() =>
+                                handleRemoveItem(Meals.id, Item.id)
+                              }
+                              className={buttonClass}
+                            >
+                              Remove Item
+                            </button>
+                          </div>
+                        </div>
+                        <div className="flex justify-around w-[50%] m-auto p-3">
+                          <p className="text-white"> Macros in Gms</p>
+                          <input
+                            value={Item.Protien === 0 ? "" : Item.Protien}
+                            placeholder="Protien"
+                            onChange={(e) =>
+                              onChangeHandle(
+                                "Protien",
+                                e.target.value,
+                                Meals.id,
+                                Item.id
+                              )
+                            }
+                            className={inputClass}
+                            required
+                            type="number"
+                          />
+                          <input
+                            value={Item.Carbs === 0 ? "" : Item.Carbs}
+                            placeholder="Carbs"
+                            onChange={(e) =>
+                              onChangeHandle(
+                                "Carbs",
+                                e.target.value,
+                                Meals.id,
+                                Item.id
+                              )
+                            }
+                            className={inputClass}
+                            required
+                            type="number"
+                          />
+                          <input
+                            value={Item.Fats === 0 ? "" : Item.Fats}
+                            placeholder="Fats"
+                            onChange={(e) =>
+                              onChangeHandle(
+                                "Fats",
+                                e.target.value,
+                                Meals.id,
+                                Item.id
+                              )
+                            }
+                            className={inputClass}
+                            required
+                            type="number"
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+        {navInput === 4 && (
+          <div>
+            <DisplayDiet meal={meals} />
+          </div>
+        )}
+        <div className="w-[50%] flex justify-around m-auto">
+          <button
+            type="button"
+            className={buttonClass}
+            onClick={onPrevHandle}
+            disabled={navInput === 1}
+          >
+            Prev
+          </button>
+          <button type="submit" className={buttonClass}>
+            Next
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UserInput;
